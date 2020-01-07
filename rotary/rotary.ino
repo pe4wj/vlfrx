a71c4589ec1f0d6a65c16b02fc28a7cbff8d7c05
pe4wj <wjubbels@gmail.com>
2020-01-07 15:07:46 +0100

    included AGC and 1 kHz steps

---
+++ rotary/rotary.ino
@@ -0,0 +1,37 @@
+/*
+ * Example using the Rotary library, dumping integers to the serial
+ * port. The integers increment or decrement depending on the direction
+ * of rotation.
+ *
+ * This example uses interrupts rather than polling.
+ */
+
+#include <Rotary.h>
+
+// Rotary encoder is wired with the common to ground and the two
+// outputs to pins 2 and 3.
+Rotary rotary = Rotary(1, 0);
+
+// Counter that will be incremented or decremented by rotation.
+int counter = 0;
+
+void setup() {
+  Serial.begin(9600);
+  attachInterrupt(0, rotate, CHANGE);
+  attachInterrupt(1, rotate, CHANGE);
+}
+
+void loop() {
+}
+
+// rotate is called anytime the rotary inputs change state.
+void rotate() {
+  unsigned char result = rotary.process();
+  if (result == DIR_CW) {
+    counter++;
+    Serial.println(counter);
+  } else if (result == DIR_CCW) {
+    counter--;
+    Serial.println(counter);
+  }
+}
--- vlfrx.ino
+++ vlfrx.ino
@@ -7,6 +7,15 @@
 #include <SPI.h>
 #include <SD.h>
 #include <SerialFlash.h>
+#include <Encoder.h>
+#include <U8g2lib.h>
+#include <Bounce.h>
+
+
+#include <Rotary.h>
+
+
+#define T4
 
 int led = 13;
 
@@ -40,24 +49,41 @@ AudioConnection          patchCord8(fir2, 0, multiply4, 0);
 // AudioConnection          patchCord7(multiply1, 0, multiply3, 0);
 // AudioConnection          patchCord8(multiply2, 0, multiply4, 0);
 
-AudioConnection          patchCord9(sine4, 0, multiply4, 1);
-AudioConnection          patchCord10(sine3, 0, multiply3, 1);
+AudioConnection          patchCord9(sine3, 0, multiply4, 1);
+AudioConnection          patchCord10(sine4, 0, multiply3, 1);
 AudioConnection          patchCord11(multiply3, 0, mixer1, 0);
 AudioConnection          patchCord12(multiply4, 0, mixer1, 1);
 AudioConnection          patchCord13(mixer1, 0, i2s2, 0);
+AudioConnection          patchCord14(mixer1, 0, i2s2, 1);
 // AudioConnection          patchCord13(sine4, 0, i2s2, 0);
+
+AudioAnalyzeRMS      rms;
+AudioConnection          patchCord15(mixer1, 0, rms, 0);
 // GUItool: end automatically generated code
 AudioControlSGTL5000     sgtl5000_1;
 
+Bounce bouncer = Bounce( 2,50);  // rotary push button
+
+U8G2_SSD1306_64X32_1F_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+
+Rotary rotary = Rotary(1, 0);
 
 // const int myInput = AUDIO_INPUT_LINEIN; // use line input on the audio shield
 const int myInput = AUDIO_INPUT_MIC; // use mic input on the audio shield
 
 
+const int samplerate = 44117.64706; //352800; //192000;
+const float samplerate_factor = 44117.64706 / samplerate;
+
 const float phaseshift = 90; // degrees phaseshift
-const float freq = 19600; // receiver center freq in Hz
+
+const float default_freq = 17200;
+float freq = default_freq; // receiver center freq in Hz
 const float test_freq = 17200; // receiver center freq in Hz
 const float bfo_freq = 600; // BFO freq in Hz
+float freqstep = 25; // tuning step in Hz
+const int micgain = 63; // mic gain in dB
+bool do_display = true;
 
 /*
 
@@ -293,10 +319,10 @@ void setup() {
   // Enable the audio shield, select input, and enable output
   sgtl5000_1.enable();
   sgtl5000_1.inputSelect(myInput);
-  sgtl5000_1.volume(0.8);
+  sgtl5000_1.volume(0);
   sgtl5000_1.lineInLevel(15);
   sgtl5000_1.lineOutLevel(13);
-  sgtl5000_1.micGain(63);
+  sgtl5000_1.micGain(micgain);
 
   // disable ADC High pass filter to reduce noise (as per http://openaudio.blogspot.com/2017/03/teensy-audio-board-self-noise.html?m=1)
   sgtl5000_1.adcHighPassFilterDisable();
@@ -305,31 +331,32 @@ void setup() {
   //setup everything
   // disable interrupts for a while to setup everything, and guarantee accurate phase quadrature
   AudioNoInterrupts();  // disable audio library momentarily
-
+  // set sample rate
+  setI2SFreq(samplerate);
   
   
   // oscillators
   // ddc sin
   sine1.amplitude(1.0);
-  sine1.frequency(freq);
+  sine1.frequency(freq * samplerate_factor);
   sine1.phase(0);
   // ddc cos
   sine2.amplitude(1.0);
-  sine2.frequency(freq);
+  sine2.frequency(freq * samplerate_factor);
   sine2.phase(phaseshift);
   // bfo sin
   sine3.amplitude(1.0);
-  sine3.frequency(bfo_freq);
+  sine3.frequency(bfo_freq * samplerate_factor);
   sine3.phase(phaseshift);
   // bfo cos
   sine4.amplitude(1.0);
-  sine4.frequency(bfo_freq);
+  sine4.frequency(bfo_freq * samplerate_factor);
   sine4.phase(0);
 
   
   // testsig cos
   sine5.amplitude(1.0);
-  sine5.frequency(test_freq);
+  sine5.frequency(test_freq * samplerate_factor);
   sine5.phase(phaseshift);
   AudioInterrupts();    // enable audio library
   
@@ -343,20 +370,250 @@ void setup() {
   // initialize the LED digital pin as an output.
   pinMode(led, OUTPUT);
 
+  // encoder, switch and LED pins
+  pinMode(0, INPUT_PULLUP); // encoder A
+  pinMode(1, INPUT_PULLUP); // encoder B
+  pinMode(2, INPUT); // encoder SW
+  pinMode(3, OUTPUT); // LED BLUE
+  pinMode(4, OUTPUT); // LED GREEN
+  pinMode(5, OUTPUT); // LED RED
+  
+  digitalWrite(3, HIGH); // LED OFF
+  digitalWrite(4, HIGH); // LED OFF
+  digitalWrite(5, HIGH); // LED OFF
+
 
 
 //start FIR filter
 
   fir1.begin(filter_taps, FILTER_TAP_NUM);
   fir2.begin(filter_taps, FILTER_TAP_NUM);
+
+  Serial.begin(9600);
+  attachInterrupt(0, rotate, CHANGE);
+  attachInterrupt(1, rotate, CHANGE);
+  Serial.println("samplerate_factor:");
+  Serial.println(samplerate_factor);
+  u8g2.begin();
+
+  // init the frequency
+  setfreq();
+  // display the frequency
+  displayfreq();
+
+
+
+  
 }
+
+
+  void setfreq(){  
+// Serial.println(freq);
+ 
+ 
+
+    
+   AudioNoInterrupts();    // enable audio library
+      // ddc sin
+  sine1.amplitude(1.0);
+  sine1.frequency(freq * samplerate_factor);
+  sine1.phase(0);
+  // ddc cos
+  sine2.amplitude(1.0);
+  sine2.frequency(freq * samplerate_factor);
+  sine2.phase(phaseshift);
+    AudioInterrupts();    // enable audio library
+  }
+
+  void displayfreq(){
+  u8g2.clearBuffer();          // clear the internal memory
+  u8g2.setFont(u8g2_font_t0_11_tf);  // choose a suitable font
+  u8g2.drawStr(0, 10, "Freq (Hz):");  // write something to the internal memory
+  u8g2.setCursor(0, 24);
+  u8g2.print(freq);
+  u8g2.sendBuffer();          // transfer internal memory to the display
+  }
+  void clear_display(){
+  u8g2.clearBuffer();          // clear the internal memory
+//  u8g2.setFont(u8g2_font_t0_11_tf);  // choose a suitable font
+//  // u8g2.drawStr(0, 10, "Hello,");  // write something to the internal memory
+//  u8g2.setCursor(0, 24);
+//  u8g2.print(freq);
+  u8g2.sendBuffer();          // transfer internal memory to the display
+  }
+
+// rotate is called anytime the rotary inputs change state.
+void rotate() {
+  unsigned char result = rotary.process();
+
+if (digitalRead(2)) {
+  freqstep = 1000;
+  }
+else {
+  freqstep = 25;
+}
+  
+  if (result == DIR_CW) {
+    freq = freq + freqstep;
+
+  } else if (result == DIR_CCW) {
+    freq = freq - freqstep;
+
+  }
+
+// frequency limits
+    if (freq < 1000) {
+      freq = 1000;
+      
+    }
+    else {
+      
+    }
+    if (freq > samplerate/2) {
+      freq = 22050;//floor(samplerate/2);
+      
+    }
+    else {
+      
+    }
+
+    
+  setfreq();
+  if (do_display) {
+    displayfreq();
+    }
+//    else {
+//      clear_display();
+//      }
+
+  }
+
+// set samplerate code by Frank Bösing
+void setI2SFreq(int freq) {
+#if defined(T4) 
+  // PLL between 27*24 = 648MHz und 54*24=1296MHz
+  int n1 = 4; //SAI prescaler 4 => (n1*n2) = multiple of 4
+  int n2 = 1 + (24000000 * 27) / (freq * 256 * n1);
+  double C = ((double)freq * 256 * n1 * n2) / 24000000;
+
+  int nfact = C;
+  int ndiv = 10000;
+  int nmult = C * ndiv - (nfact * ndiv);
+
+  CCM_ANALOG_PLL_AUDIO = 0;
+  CCM_ANALOG_PLL_AUDIO |= CCM_ANALOG_PLL_AUDIO_ENABLE
+                          | CCM_ANALOG_PLL_AUDIO_POST_DIV_SELECT(2) // 2: 1/4; 1: 1/2; 0: 1/1
+                          | CCM_ANALOG_PLL_AUDIO_DIV_SELECT(nfact);
+
+  CCM_ANALOG_PLL_AUDIO_NUM   = nmult & CCM_ANALOG_PLL_AUDIO_NUM_MASK;
+  CCM_ANALOG_PLL_AUDIO_DENOM = ndiv & CCM_ANALOG_PLL_AUDIO_DENOM_MASK;
+  while (!(CCM_ANALOG_PLL_AUDIO & CCM_ANALOG_PLL_AUDIO_LOCK)) {}; //Wait for pll-lock
+
+  const int div_post_pll = 1; // other values: 2,4
+  CCM_ANALOG_MISC2 &= ~(CCM_ANALOG_MISC2_DIV_MSB | CCM_ANALOG_MISC2_DIV_LSB);
+  if (div_post_pll > 1) CCM_ANALOG_MISC2 |= CCM_ANALOG_MISC2_DIV_LSB;
+  if (div_post_pll > 3) CCM_ANALOG_MISC2 |= CCM_ANALOG_MISC2_DIV_MSB;
+
+  CCM_CSCMR1 = (CCM_CSCMR1 & ~(CCM_CSCMR1_SAI1_CLK_SEL_MASK))
+               | CCM_CSCMR1_SAI1_CLK_SEL(2); // &0x03 // (0,1,2): PLL3PFD0, PLL5, PLL4
+  CCM_CS1CDR = (CCM_CS1CDR & ~(CCM_CS1CDR_SAI1_CLK_PRED_MASK | CCM_CS1CDR_SAI1_CLK_PODF_MASK))
+               | CCM_CS1CDR_SAI1_CLK_PRED(n1 - 1) // &0x07
+               | CCM_CS1CDR_SAI1_CLK_PODF(n2 - 1); // &0x3f
+
+  IOMUXC_GPR_GPR1 = (IOMUXC_GPR_GPR1 & ~(IOMUXC_GPR_GPR1_SAI1_MCLK1_SEL_MASK))
+                    | (IOMUXC_GPR_GPR1_SAI1_MCLK_DIR | IOMUXC_GPR_GPR1_SAI1_MCLK1_SEL(0));  //Select MCLK
+
+  //Serial.printf("n1:%d n2:%d C:%f c0:%d c1:%d c2:%d\n",  n1, n2, C, nfact, nmult, ndiv);
+#else
+  typedef struct {
+    uint8_t mult;
+    uint16_t div;
+  } tmclk;
+
+  const int numfreqs = 20;
+  //  const int samplefreqs[numfreqs] = { 8000, 11025, 16000, 22050, 32000, 44100, (int)44117.64706 , 48000, 88200, (int)44117.64706 * 2, 96000, 100000, 176400, (int)44117.64706 * 4, 192000};
+  const int samplefreqs[numfreqs] = { 8000, 11025, 16000, 22050, 32000, 44100, (int)44117.64706 , 48000, 50223, 88200, (int)44117.64706 * 2,
+                                      96000, 100000, 100466, 176400, (int)44117.64706 * 4, 192000, 234375, 281000, 352800
+                                    };
+
+#if (F_PLL==180000000)
+  //{ 8000, 11025, 16000, 22050, 32000, 44100, (int)44117.64706 , 48000,
+  const tmclk clkArr[numfreqs] = {{46, 4043}, {49, 3125}, {73, 3208}, {98, 3125}, {183, 4021}, {196, 3125}, {16, 255}, {128, 1875},
+    // 50223, 88200, (int)44117.64706 * 2, 96000, 100466, 176400, (int)44117.64706 * 4, 192000, 234375, 281000, 352800};
+    {1, 14}, {107, 853}, {32, 255}, {219, 1604}, {224, 1575}, {1, 7}, {214, 853}, {64, 255}, {219, 802}, {1, 3}, {2, 5} , {1, 2}
+  };
+#endif
+
+
+  for (int f = 0; f < numfreqs; f++) {
+    if ( freq == samplefreqs[f] ) {
+      while (I2S0_MCR & I2S_MCR_DUF) ;
+      I2S0_MDR = I2S_MDR_FRACT((clkArr[f].mult - 1)) | I2S_MDR_DIVIDE((clkArr[f].div - 1));
+      return;
+    }
+  }
+#endif //Teensy4
+}
+
 
 void loop() {
 
-  delay(200);  
-  digitalWrite(led, HIGH);
-  delay(200);  
-  digitalWrite(led, LOW);
+//crude AGC
+if (rms.available()) {
+      
+      float agc = 0.5 /rms.read();
+      if (agc > 0.7) {
+        agc = 0.7;
+      }
+      sgtl5000_1.volume(agc);
+      Serial.println(agc);
+}
+
+  
+bouncer.update ( );
+ int value = bouncer.risingEdge();//bouncer.read();
+//  long newPosition = myEnc.read();
+//  if (newPosition != oldPosition) {
+//    oldPosition = newPosition;
+    
+
+//  delay(200);  
+//  digitalWrite(led, HIGH);
+//  // digitalWrite(5, LOW); // LED ON
+//  delay(200);  
+//  digitalWrite(led, LOW);
+  // digitalWrite(5, HIGH); // LED OFF
+//  
+// if (digitalRead(2)) {
+//   digitalWrite(4, LOW); // LED ON
+//   // freq = default_freq;
+//   // setfreq();
+//   // freqstep = 1000;
+//   
+// } else {
+//   digitalWrite(4, HIGH); // LED OFF
+//   // freqstep = 25;
+//   
+// }
+
+if (value == HIGH) {
+
+  if (do_display){
+    do_display = false;
+    digitalWrite(4, HIGH); // LED OFF 
+    digitalWrite(5, HIGH); // LED OFF 
+    
+       
+
+  }
+  else {
+    do_display = true;
+    digitalWrite(4, LOW); // LED ON
+    digitalWrite(5, LOW); // LED ON
+
+  }
+}
+  
   
   
 }
